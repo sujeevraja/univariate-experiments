4316a40690281912ff65e97625d90acf31d49e0e
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2020-02-21 01:04:09 -0700

    test package API

--- src/UnivariateExperiments.jl
+++ src/UnivariateExperiments.jl
@@ -1,8 +1,10 @@
 module UnivariateExperiments
 
+using PolyhedralRelaxations
+
 function greet()
-    @debug "Hello, world!"
-    @info "Hello, world!"
+    formulation_data, function_data = construct_milp_relaxation(x -> x^3, collect(-1.0:1.0:1.0))
+    @info formulation_data
 end
 
 end # module
