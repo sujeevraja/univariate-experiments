95a3bdfbec0a2835412d90b29f5293a9bd1c4ac1
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2020-02-22 18:47:19 -0700

    add convex hull plot

--- src/UnivariateExperiments.jl
+++ src/UnivariateExperiments.jl
@@ -40,7 +40,7 @@ function get_mip_values()::Tuple{Vector{Float64},Vector{Float64},Vector{Float64}
     A, b = PR.get_leq_constraint_matrices(formulation_data)
     @constraint(milp, A * x .<= b)
 
-    # Set objective sense and objective coefficients that don't change.
+    # Add objective.
     set_objective_function(milp, x[formulation_data.y_index])
 
     # Collect solutions from different linear objectives.
@@ -78,4 +78,52 @@ function plot_mip()
     fs = [x^3 for x in xs]
     p = Plots.plot(xs,hcat(max_ys,fs,min_ys))
     Plots.savefig(p, "out/mip.pdf")
+end
+
+function plot_lp()
+    f = x -> x^3
+    xs = collect(-1.0:0.01:1.0)
+    fs = [x^3 for x in xs]
+    p = Plots.plot(xs,fs,legend=false)
+
+    cbc_opt = optimizer_with_attributes(Cbc.Optimizer, "logLevel" => 0)
+    lp_data, fn_data = PR.construct_lp_relaxation(f, collect(-1.0:0.25:1.0))
+    lp = Model(cbc_opt)
+    lb, ub = PR.get_variable_bounds(lp_data)
+    num_vars = PR.get_num_variables(lp_data)
+    @variable(lp, lb[i] <= y[i=1:num_vars] <= ub[i])
+    A, b = PR.get_eq_constraint_matrices(lp_data)
+    @constraint(lp, A * y .== b)
+
+    y_min, y_max = -1.0,1.0
+
+    α_max = π/2
+    res = π/100
+    for α ∈ collect(res:res:(α_max-res))
+        m = tan(α)
+        xvar = y[lp_data.x_index]
+        yvar = y[lp_data.y_index]
+
+        @objective(lp, Max, yvar - (xvar * tan(α)))
+        optimize!(lp)
+        @assert termination_status(lp) == MOI.OPTIMAL
+        c = objective_value(lp)
+        x_min = max((y_min - c) / m, -1.0)
+        x_max = min((y_max - c) / m, 1.0)
+        line_xs = [x_min, x_max]
+        line_ys = [(m*x) + c for x in line_xs]
+        Plots.plot!(line_xs, line_ys)
+
+        @objective(lp, Min, yvar - (xvar * tan(α)))
+        optimize!(lp)
+        @assert termination_status(lp) == MOI.OPTIMAL
+        c = objective_value(lp)
+        x_min = max((y_min - c) / m, -1.0)
+        x_max = min((y_max - c) / m, 1.0)
+        line_xs = [x_min, x_max]
+        line_ys = [(m*x) + c for x in line_xs]
+        Plots.plot!(line_xs, line_ys)
+    end
+
+    Plots.savefig(p, "out/lp.pdf")
 end
