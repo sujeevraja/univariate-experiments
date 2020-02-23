8d6415a5675c352178b97cbffabe0dd0e5bcf941
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2020-02-23 08:29:35 -0700

    cleanup function, dependencies

--- src/UnivariateExperiments.jl
+++ src/UnivariateExperiments.jl
@@ -1,7 +1,6 @@
 import Cbc
 import Plots
 import PolyhedralRelaxations
-using Printf
 using JuMP
 using SparseArrays
 
@@ -14,35 +13,34 @@ Plots.gr()
 base_partition = collect(-1.0:0.25:1.0)
 
 function get_mip_values()::Tuple{Vector{Float64},Vector{Float64},Vector{Float64}}
-    f = x->x^3
-    f_dash = x->3*(x^2)
-    formulation_data, function_data = PR.construct_milp_relaxation(f,f_dash,base_partition)
     cbc_opt = optimizer_with_attributes(Cbc.Optimizer, "logLevel" => 0)
     milp = Model(cbc_opt)
-    lb, ub = PR.get_variable_bounds(formulation_data)
+    f = x->x^3
+    milp_data, function_data = PR.construct_milp_relaxation(f,base_partition)
+    lb, ub = PR.get_variable_bounds(milp_data)
 
-    @info "x bounds: $(lb[formulation_data.x_index]), $(ub[formulation_data.x_index])"
-    @info "y bounds: $(lb[formulation_data.y_index]), $(ub[formulation_data.y_index])"
+    @info "x bounds: $(lb[milp_data.x_index]), $(ub[milp_data.x_index])"
+    @info "y bounds: $(lb[milp_data.y_index]), $(ub[milp_data.y_index])"
 
     # Add variables
-    num_vars = PR.get_num_variables(formulation_data)
-    binary_indices, _ = findnz(formulation_data.binary)
+    num_vars = PR.get_num_variables(milp_data)
+    binary_indices = PR.get_binary_indices(milp_data)
     @variable(milp, lb[i] <= x[i=1:num_vars] <= ub[i])
     for k in binary_indices
         set_binary(x[k])
     end
     for k in 1:num_vars
-        set_name(x[k], PR.get_variable_names(formulation_data)[k])
+        set_name(x[k], PR.get_variable_names(milp_data)[k])
     end
 
     # Add constraints
-    A, b = PR.get_eq_constraint_matrices(formulation_data)
+    A, b = PR.get_eq_constraint_matrices(milp_data)
     @constraint(milp, A * x .== b)
-    A, b = PR.get_leq_constraint_matrices(formulation_data)
+    A, b = PR.get_leq_constraint_matrices(milp_data)
     @constraint(milp, A * x .<= b)
 
     # Add objective.
-    set_objective_function(milp, x[formulation_data.y_index])
+    set_objective_function(milp, x[milp_data.y_index])
 
     # Collect solutions from different linear objectives.
     xs = collect(-1.0:0.01:1.0)
@@ -50,22 +48,21 @@ function get_mip_values()::Tuple{Vector{Float64},Vector{Float64},Vector{Float64}
     min_ys = Float64[]
 
     for p in xs
-        set_lower_bound(x[formulation_data.x_index], p)
-        set_upper_bound(x[formulation_data.x_index], p)
+        set_lower_bound(x[milp_data.x_index], p)
+        set_upper_bound(x[milp_data.x_index], p)
 
         set_objective_sense(milp, MOI.MAX_SENSE)
         optimize!(milp)
         @assert termination_status(milp) == MOI.OPTIMAL
-        yval = value.(x[formulation_data.y_index])
+        yval = value.(x[milp_data.y_index])
         obj = objective_value(milp)
         @assert isapprox(yval, obj, atol=1e-5) "obj $obj and y $yval different in max problem"
         push!(max_ys, yval)
-        # @printf "x %.3f y %.3f max obj %.3f\n" p yval objective_value(milp)
 
         set_objective_sense(milp, MOI.MIN_SENSE)
         optimize!(milp)
         @assert termination_status(milp) == MOI.OPTIMAL
-        yval = value.(x[formulation_data.y_index])
+        yval = value.(x[milp_data.y_index])
         obj = objective_value(milp)
         @assert isapprox(yval, obj, atol=1e-5) "obj $obj and y $yval different in min problem"
         push!(min_ys, yval)
@@ -87,8 +84,8 @@ function plot_lp()
     fs = [x^3 for x in xs]
     p = Plots.plot(xs,fs,legend=false,color=:orange)
 
-    cbc_opt = optimizer_with_attributes(Cbc.Optimizer, "logLevel" => 0)
     lp_data, fn_data = PR.construct_lp_relaxation(f, base_partition)
+    cbc_opt = optimizer_with_attributes(Cbc.Optimizer, "logLevel" => 0)
     lp = Model(cbc_opt)
     lb, ub = PR.get_variable_bounds(lp_data)
     @info "x bounds: $(lb[lp_data.x_index]), $(ub[lp_data.x_index])"
@@ -99,11 +96,16 @@ function plot_lp()
     A, b = PR.get_eq_constraint_matrices(lp_data)
     @constraint(lp, A * y .== b)
 
+    for k in 1:num_vars
+        set_name(y[k], PR.get_variable_names(lp_data)[k])
+    end
+
     y_min, y_max = -1.0,1.0
 
-    α_max = π/2
     res = π/100
-    for α ∈ collect(res:res:(α_max-res))
+    α_min = res
+    α_max = (π/2) - res
+    for α ∈ collect(α_min:res:α_max)
         m = tan(α)
         xvar = y[lp_data.x_index]
         yvar = y[lp_data.y_index]
